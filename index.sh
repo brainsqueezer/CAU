65c99309fabfbf50ed1a3c516f9f31614fc03c80
Ramon Antonio Parada <rap@ramonantonio.net>
2011-03-05 21:03:37 +0000

    jjj

--- index.sh
+++ index.sh
@@ -4,6 +4,7 @@ exit=0
 nombre="SinNombre"
 nivel=0
 experiencia=0
+personaje=0
 
 #Hoja Personaje
 #Niveles <- modos de subir: subes de modo general, eliges usando tu experiencia, lo que mas hayas usado es lo que sube
@@ -23,7 +24,76 @@ function procesar_cmd {
    fi
 }
 
-#CAU = Centro de Asesoramiento de Usuarios
+function valor_habilidad {
+RANGE=18
+number=$RANDOM
+let "number %= $RANGE"
+return $number
+}
+
+echo
+echo
+echo "................................................................................"
+echo ".......77+OO...................................................................."
+echo ".....,I77I?OO+.................................................................."
+echo "..:I777I77+OZZ:................................................................."
+echo ".:7I7I7III?ZZOO.................................................$=.............."
+echo "..:777777=ZOOOO:...:+.D:7.$=8,O=8.~8:?.+,7.,?~,,+?,?,.7.7.O=8,~8$=8,7.Z=8,7.$=.."
+echo "...=7777I.OOOOO:...:=.8~?.7+,O7.8,:8:+.:.7..I=,.II.+,.I.I.Z+8,:8$=8,I.=:8,7.7+.."
+echo "...,7777+~OOOOZ.......:.,=,..:...~:..,:...:,.,.:..,.::..:~Z+.~:.,.~.,~,.~.,~,..."
+echo "...,I7I7.ZOZOO:.........................................~I~....................."
+echo "...~?,..7OOOZ+.................................................................."
+echo "................................................................................"
+echo
+echo
+echo "Como te llamas?"
+read nombre
+echo
+echo
+echo
+echo "Hola $nombre. Bienvenido/a al CAU"
+echo "Centro de Asesoramiento a Usuarios de NovaCaixaGalicia"
+echo "Tienes 8 horas de tiempo y 100 euros para resolver las misiones de hoy."
+echo "Aunque puedes ganar 1 hora si evitas ir a comer y otra hora si te vas con la señora de la limpieza."
+echo
+echo "Elije tu personaje:"
+echo "1. Tecnico hardware"
+echo "2. Programador de aplicaciones"
+echo ""
+read personaje
+
+
+echo "Generando tus habilidades..."
+valor_habilidad
+h_cables=$?
+echo "Enchufar cables -> $h_cables/18"
+valor_habilidad
+h_scripts=$?
+echo "Crear Scripts -> $h_scripts/18"
+valor_habilidad
+h_usar=$?
+echo "Usar cualquier programa aleatorio -> $h_usar/18"
+valor_habilidad
+h_soportar=$?
+echo "Soportar usuarios -> $h_soportar/18"
+valor_habilidad
+h_mentiras=$?
+echo "Detectar mentiras del usuario -> $h_mentiras/18"
+valor_habilidad
+h_sueno=$?
+echo "Soportar la falta de sueño -> $h_sueno/18"
+valor_habilidad
+h_hardware=$?
+echo "Encontrar errores hardware -> $h_hardware/18"
+echo
+echo "Estás preparado?"
+read
+echo
+echo
+echo
+echo "Hay cinco telefonos sonando. ¿Cual deseas responder? [1-5]"
+read telefono
+
 while [ $exit -eq 0 ]; do
    echo     "Operador> CAU Buenos dias. En que puedo ayudarle?"
    echo -ne "cmd>      "
