81a33b7b5ecd47ada5100df470f4062c8c5db31c
Ramon Antonio Parada <rap@ramonantonio.net>
2011-03-05 20:29:43 +0000

    jjj

---
+++ misiones.sh
@@ -0,0 +1,10 @@
+Misiones
+
+
+
+4. Hacer café
+5. Programar ERP
+6. Instalar ActiveDirectory
+7. Reparar fallo desconocido de la red
+8. Lidiar con usuario cabreado que no tiene ningun problema
+9. Compilar una Gentoo entera
